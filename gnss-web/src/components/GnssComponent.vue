a598903f878f590f19b599ebe0d7a08a148abf8c
anpopov <anpopov@tectus-it.ru>
2021-07-16 14:51:07 +0300

    GNSS-5: added show data by day

--- gnss-web/src/components/GnssComponent.vue
+++ gnss-web/src/components/GnssComponent.vue
@@ -4,7 +4,7 @@
             Назад
         </button>
             <vuetable ref="vuetable"
-                      api-url="http://localhost:8080/api/files/years/"
+                      api-url="http://lab-gnss.bmstu.ru/api/files/years/"
                       :fields="['year']"
                       data-path=""
                       pagination-path=""
@@ -36,6 +36,9 @@
                 } else if (this.date.year && this.date.month && !this.date.day) {
                     this.$refs.vuetable.apiUrl = this.apiUrl + this.date.year + '/' + this.date.month + "/days/";
                     this.$refs.vuetable.fields = ['year', 'month', 'day'];
+                } else if (this.date.year && this.date.month && this.date.day) {
+                    this.$refs.vuetable.apiUrl = this.apiUrl + this.date.year + '/' + this.date.month + "/" + this.date.day + "/";
+                    this.$refs.vuetable.fields = ['id', 'name', 'year', 'month', 'day'];
                 }
                 this.$refs.vuetable.refresh();
             },
